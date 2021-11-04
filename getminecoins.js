033649e211a60fd3694b3e102d5550d9a7de5b96
xdjer <fran.jeremy@protonmail.com>
2021-11-04 23:13:14 +0800

    First commit

---
+++ getminecoins.js
@@ -0,0 +1,45 @@
+console.clear();
+
+require('dotenv').config();
+const discord = require('discord.js');
+const p = require('phin')
+
+const intents = new discord.Intents(32767);
+
+const client = new discord.Client({
+    intents
+});
+
+// on bot start
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+
+});
+
+client.on('messageCreate', msg => {
+    // ignore messesage if it is from bot
+    if (msg.author.bot) return;
+
+    // ignore messages that don't start with the prefix
+    if (msg.content.indexOf(process.env.PREFIX) !== 0) return;
+
+    // get the command and the args
+    const args = msg.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    // get the command
+    if (command === 'download') {
+        async function getDownloadLink() {
+            const res = await p({
+                url: 'https://getminecoins.com/generate_download.php?key=' + process.env.API_KEY,
+                parse: 'json',
+            });
+            if (res.body) {
+                msg.reply(res.body.url);
+            }
+        }
+        getDownloadLink();
+    };
+});
+
+client.login(process.env.TOKEN);
